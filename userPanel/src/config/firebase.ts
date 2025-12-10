f391d7d52f2cd2a275fd5896d608a8b0b6ef30a3
Sachin Singh <sachinsingh9272@gmail.com>
2025-12-10 12:56:15 +0530

    Updated Firebase Configuration

--- userPanel/src/config/firebase.ts
+++ userPanel/src/config/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyChf-IdA1voB5MKgSWHzZbYj4tEYa0c9AM",
-    authDomain: "askape-bc0e0.firebaseapp.com",
-    projectId: "askape-bc0e0",
-    storageBucket: "askape-bc0e0.firebasestorage.app",
-    messagingSenderId: "76499360106",
-    appId: "1:76499360106:web:2f965dca0accf4e1acad13",
-    measurementId: "G-7QYVKD4CQW"
+    apiKey: "AIzaSyCv9_NKsd8-roI5sf5xex8e0dD-EZsQ_F0",
+    authDomain: "askape-cef74.firebaseapp.com",
+    projectId: "askape-cef74",
+    storageBucket: "askape-cef74.firebasestorage.app",
+    messagingSenderId: "745415698584",
+    appId: "1:745415698584:web:9a6e0f24048fe7396b08ed",
+    measurementId: "G-DPE7FWE2TX"
 };
 
 // Initialize Firebase
