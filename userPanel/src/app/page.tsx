82d54cd134302fe3c6b36953dfa824cb91bbd61f
Sachin Singh <sachinsingh9272@gmail.com>
2025-12-19 18:26:29 +0530

    Updated pages

--- userPanel/src/app/page.tsx
+++ userPanel/src/app/page.tsx
@@ -1046,7 +1046,7 @@ function ChatContent() {
 
       {/* Toast */}
       <div className={`toast ${toast.show ? 'show' : ''}`}>
-        <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" className="w-5 h-5">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
         </svg>
         <span>{toast.message}</span>
